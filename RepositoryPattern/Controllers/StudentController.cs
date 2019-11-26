c379a958e2c635bd61a6cf7fc0908bbbb71808ad
Sarath baiju <sarath.bu@pitsolutions.com>
2019-11-26 16:00:00 +0530

    test

--- RepositoryPattern/Controllers/StudentController.cs
+++ RepositoryPattern/Controllers/StudentController.cs
@@ -19,7 +19,7 @@ namespace RepositoryPattern.Controllers
            // some();
             return View(_repository.GetStudents());
         }
-        [Queue("Students")]
+        [Queue("Students-test")]
         private Expression<Action<string>> some()
         {
             for (int i = 0; i < 10; i++)
